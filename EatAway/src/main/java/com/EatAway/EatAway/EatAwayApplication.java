12cd929ee246d8bcc97a17985498255f717161a5
WendyBadilla <117774092+WendyBadilla@users.noreply.github.com>
2024-07-21 17:27:40 -0600

    Creacion del proyecto

---
+++ EatAway/src/main/java/com/EatAway/EatAway/EatAwayApplication.java
@@ -0,0 +1,13 @@
+package com.EatAway.EatAway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatAwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatAwayApplication.class, args);
+	}
+
+}
---
+++ EatAway/src/test/java/com/EatAway/EatAway/EatAwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EatAway.EatAway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatAwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
